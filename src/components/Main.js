f504d784d002f5dc657c51d84055f8689fcdab70
android1816 <android1816@github.com>
2021-07-26 11:23:30 +0700

    v1-26/7/2021

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,12 @@ const Main = (props) => {
     <div className="main">
       <iframe
         width="100%"
-        height="600px"
-        src="https://www.youtube.com/embed/K3beVcHIGTs"
+        src="https://www.youtube.com/embed/DIgv-e18OzA?rel=0&autoplay=1"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
+        style={{display:"inline-block", height:"100%"}}
       ></iframe>
       {/* <WebcamCapture check={capture}/> */}
     </div>
